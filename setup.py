d6f007a3be186e563dde55de128813e4ebceaa92
Юрий Пайков <cry5tal@cry5tal.in>
2016-12-26 17:21:45 +0300

    verison 0.0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='python-redis-rate-limit',
-    version='0.0.1',
+    version='0.0.2',
     description=u'',
     long_description=readme,
     author=u'Victor Torres',
